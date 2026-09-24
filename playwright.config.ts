d6f8be18fa31b489afc4d9801a542ba9b4e9de91
Sinem <sinemayk@gmail.com>
2026-09-24 18:55:23 +0200

    Allure reports added

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,14 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ reporter: [
+    ["line"], //terminalde test sonuclarini anlik olarak satir satir gosterir
+    ["html"], //pw nin kendi built-in HTML raporunu olusturur ve test bitince otomatik acar
+    [
+      "allure-playwright", //Allure raporlama kutuphanesini kullan demek
+      { resultsDir: "allure-results" },
+    ], //test sonuclarinin ham verisini "allure-results" klasorune kaydet
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -30,6 +37,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
--- src/ui/login.spec.ts
+++ src/ui/login.spec.ts
@@ -1,25 +1,18 @@
-import { test } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
-import { InventoryPage } from '../pages/InventoryPage';
+import { test } from "../../src/fixtures/page-fixtures";
 
 test.describe("Login", () => {
-    test("gecerli kullanici ile giris yapar ve urun listesini gorur", async({page}) =>{
-        const loginPage = new LoginPage(page);
-        const inventoryPage = new InventoryPage(page);
+  test("gecerli kullanici ile giris yapar ve urun listesini gorur", async ({ loginPage, inventoryPage }) => {
+    await loginPage.open();
+    await loginPage.login(
+      process.env.SAUCE_USERNAME!,
+      process.env.SAUCE_PASSWORD!
+    );
+    await inventoryPage.expectItemCount(6);
+  });
 
-        await loginPage.open();
-        await loginPage.login(
-            process.env.SAUCE_USERNAME!,
-            process.env.SAUCE_PASSWORD!
-        );
-        await inventoryPage.expectItemCount(6);
-
-    });
-
-    test("gecersiz sifre ile hata mesaji gorunur", async({page}) =>{
-        const loginPage = new LoginPage(page);
-        await loginPage.open();
-        await loginPage.login(process.env.SAUCE_USERNAME!, "yanlis_sifre");
-        await loginPage.expectErrorVisible();
-     });
-})
+  test("gecersiz sifre ile hata mesaji gorunur", async ({ loginPage }) => {
+    await loginPage.open();
+    await loginPage.login(process.env.SAUCE_USERNAME!, "yanlis_sifre");
+    await loginPage.expectErrorVisible();
+  });
+});
